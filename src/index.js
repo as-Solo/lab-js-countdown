e330cee1ea75cc106167041d745600700cc51c61
as-Solo <asdelsolo@gmail.com>
2024-09-05 16:49:12 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
+let toastMessages = ["⏰ Final countdown! ⏰", "Start the engines! 💥", "Lift off! 🚀"]
 
 
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
+document.querySelector("#start-btn").addEventListener('click', ()=>{
+  startCountdown();
+})
 
 
 
@@ -14,11 +17,23 @@ let timer = null; // Variable to store the interval
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
-
-
-  // Your code goes here ...
+  showToast(toastMessages[0])
+  const timeNode = document.querySelector("#time");
+  timeNode.innerText = remainingTime
+  const timeId = setInterval(()=>{
+    remainingTime--
+    timeNode.innerText = remainingTime
+    if (remainingTime === DURATION / 2){
+      showToast(toastMessages[1])
+    }
+    
+    if (remainingTime === 0){
+      clearInterval(timeId)
+      remainingTime = DURATION
+      showToast(toastMessages[2])
+    }
+  }, 1000)
 }
-
 
 
 
@@ -26,13 +41,25 @@ function startCountdown() {
 function showToast(message) {
   console.log("showToast called!");
 
-  // Your code goes here ...
-
-
-
+  const card = document.querySelector("#toast")
+  const cardMessage = document.querySelector("#toast-message")
+  cardMessage.innerText = message
+  card.classList.toggle("show")
+  const toastId = setTimeout(()=>{
+    card.classList.toggle("show")
+    console.log("Disarmed")
+  }, 3000)
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
+  const toastButton = document.querySelector("#close-toast")
+  toastButton.addEventListener('click', ()=>{
+    clearTimeout(toastId)
+    console.log('ref1 --> ' + card.classList)
+    if (card.classList.contains("show")){
+      card.classList.toggle("show")
+    }
+    console.log("Emergency disarmed")
+    console.log('ref2 --> ' + card.classList)
 
-  // Your code goes here ...
-
+  })
 }
